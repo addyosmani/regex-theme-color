489c2c27d10667bcc1011d68fdabf5c41019c42c
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-12-31 11:15:34 +0100

    Fix identation and add editorconfig

--- test/test.js
+++ test/test.js
@@ -4,33 +4,33 @@ var assert = require('assert');
 var themeColorRegex = require('../');
 
 describe('regex-theme-color node module', function () {
-	it('should match a theme-color meta tag in a string', function (done) {
-		assert.equal(themeColorRegex().test('<meta name="theme-color" content="#3372DF">'), true);
-		assert.equal(themeColorRegex().test('<meta   name="theme-color" content="#3372DF" >'), true);
-		assert.equal(themeColorRegex().test('<meta   name="theme-color" content=\'#3372DF\' >'), true);
-		assert.equal(themeColorRegex().test('<meta name=\'theme-color\' content=\'#3372DF\'>'), true);
-		done();
-	});
+  it('should match a theme-color meta tag in a string', function (done) {
+    assert.equal(themeColorRegex().test('<meta name="theme-color" content="#3372DF">'), true);
+    assert.equal(themeColorRegex().test('<meta   name="theme-color" content="#3372DF" >'), true);
+    assert.equal(themeColorRegex().test('<meta   name="theme-color" content=\'#3372DF\' >'), true);
+    assert.equal(themeColorRegex().test('<meta name=\'theme-color\' content=\'#3372DF\'>'), true);
+    done();
+  });
 
-	it('should match a theme-color regardless of order in string', function (done) {
-		assert.equal(themeColorRegex().test('<meta content="#3372DF" name="theme-color">'), true);
-		assert.equal(themeColorRegex().test('<meta 			content="#3372DF"         name="theme-color">'), true);
-		done();
-	});
+  it('should match a theme-color regardless of order in string', function (done) {
+    assert.equal(themeColorRegex().test('<meta content="#3372DF" name="theme-color">'), true);
+    assert.equal(themeColorRegex().test('<meta content="#3372DF"         name="theme-color">'), true);
+    done();
+  });
 
-	it('should fail if a theme-color is not included in the meta tag', function (done) {
-		assert.equal(themeColorRegex().test('<meta name="">'), false);
-		assert.equal(themeColorRegex().test('<meta content>'), false);
-		assert.equal(themeColorRegex().test('<meta content="">'), false);
-		done();
-	});
-	
-	it('should fail if a theme-color does not include content', function (done) {
-		assert.equal(themeColorRegex().test('<meta name="theme-color">'), false);
-		assert.equal(themeColorRegex().test('<meta name="theme-colour">'), false);
-		assert.equal(themeColorRegex().test('<meta name="theme-color" content>'), false);
-		assert.equal(themeColorRegex().test('<meta name="theme-color" content=>'), false);
-		done();
-	});
+  it('should fail if a theme-color is not included in the meta tag', function (done) {
+    assert.equal(themeColorRegex().test('<meta name="">'), false);
+    assert.equal(themeColorRegex().test('<meta content>'), false);
+    assert.equal(themeColorRegex().test('<meta content="">'), false);
+    done();
+  });
+
+  it('should fail if a theme-color does not include content', function (done) {
+    assert.equal(themeColorRegex().test('<meta name="theme-color">'), false);
+    assert.equal(themeColorRegex().test('<meta name="theme-colour">'), false);
+    assert.equal(themeColorRegex().test('<meta name="theme-color" content>'), false);
+    assert.equal(themeColorRegex().test('<meta name="theme-color" content=>'), false);
+    done();
+  });
 });
 
